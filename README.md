634753991d1c2e110376af961340d1ef4b669b2e
Hirusha Sipsara <hirushasipsara@gmail.com>
2026-08-19 20:49:17 +0530

    ASSMS-17 add Terraform remote state bootstrap

--- README.md
+++ README.md
@@ -26,3 +26,18 @@ Checks:
 - terraform fmt
 - terraform init -backend=false
 - terraform validate
+
+## Terraform Remote State Bootstrap
+
+The bootstrap configuration initially uses local state because it creates the Azure Storage resources that will later hold remote Terraform state.
+
+1. `az login`
+2. `az account show`
+3. `cd terraform/bootstrap/tfstate`
+4. Copy `terraform.tfvars.example` to `terraform.tfvars`.
+5. Provide a globally unique Azure Storage Account name.
+6. `terraform init`
+7. `terraform fmt -check`
+8. `terraform validate`
+9. `terraform plan`
+10. `terraform apply`
--- terraform/bootstrap/tfstate/outputs.tf
+++ terraform/bootstrap/tfstate/outputs.tf
@@ -1,2 +1,19 @@
-# Placeholder
+output "resource_group_name" {
+  description = "Name of the Terraform state resource group."
+  value       = azurerm_resource_group.tfstate.name
+}
 
+output "storage_account_name" {
+  description = "Name of the Terraform state Storage Account."
+  value       = azurerm_storage_account.tfstate.name
+}
+
+output "storage_account_id" {
+  description = "Resource ID of the Terraform state Storage Account."
+  value       = azurerm_storage_account.tfstate.id
+}
+
+output "container_name" {
+  description = "Name of the private Terraform state blob container."
+  value       = azurerm_storage_container.tfstate.name
+}
